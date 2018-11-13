84ac7c8e5d0b488b7fa95994f8b5813c01c98768
HyunkaSuh <sgh415@gmail.com>
2018-11-12 23:30:12 -0600

    revised html and css

--- clock.js
+++ clock.js
@@ -1,12 +1,14 @@
-const clockContainer = document.querySelector(".js-clock"),
-      clockTitle = clockContainer.querySelector("h1");
+// const clockContainer = document.querySelector(".js-clock");
+// const clockTitle = clockContainer.querySelector("h1");
+const clockTitle = document.querySelector(".js-clock");
 
 function getTime() {
     const date = new Date();
     const hours = date.getHours();
     const minutes = date.getMinutes();
     const seconds = date.getSeconds();
-    clockTitle.innerText = `${hours <10 ? `0${hours}`: hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds <10 ? `0${seconds}` : seconds}`; // mini-if
+    clockTitle.innerText = `${hours <10 ? `0${hours}`: hours}:${minutes < 10 ? `0${minutes}` : minutes}`; // mini-if
+    // clockTitle.innerText = `${hours <10 ? `0${hours}`: hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds <10 ? `0${seconds}` : seconds}`; // mini-if
 }
 function init() {
     getTime();
