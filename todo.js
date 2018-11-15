85062ff9e4c0a9164c4f611477c8333370ef2501
HyunkaSuh <sgh415@gmail.com>
2018-11-14 23:26:27 -0600

    decoration

--- todo.js
+++ todo.js
@@ -32,12 +32,13 @@ function paintToDo(text) {
     const delBtn = document.createElement("button");
     const span = document.createElement("span");
     const newId = toDos.length + 1;
-    delBtn.innerText = "□";
+    delBtn.innerText = "✔︎";
     delBtn.addEventListener("click", deleteToDo);
     span.innerText = text;
     li.appendChild(delBtn);
     li.appendChild(span);
     li.id = newId;
+    li.className = "toDoItem";
     toDoList.appendChild(li);
     const toDoObj = {
         text: text,
